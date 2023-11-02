8de62f8ba78e86dd981658ab2a50c03ba1dbab95
sultaan-ahmed <sultanduet2019@gmail.com>
2023-11-02 07:41:49 +0600

    Projects part completed

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,12 +3,21 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import Project from "@/components/project";
 
 const Projects = () => {
   const { ref } = useSectionInView("Projects", 0.5);
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            {/* TODO: Project components add here */}
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
   );
 };
