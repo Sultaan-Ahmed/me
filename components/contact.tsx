e3fb5da4c158ba8300c312d0918c118ffb1c6abf
sultaan-ahmed <sultanduet2019@gmail.com>
2023-11-08 08:41:31 +0600

    Contact Form Section Add

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,59 @@
+"use client";
 import React from "react";
-
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail.ts";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 const Contact = () => {
-  return <div>Contact</div>;
+  const { ref } = useSectionInView("Contact");
+  return (
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
+      <SectionHeading>Contact Me</SectionHeading>
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
+        Please contact me directly at
+        <a href="mailto:example@gmail.com" className="underline">
+          example@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
+      <form
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+          if (error) {
+            return toast.error(error);
+          }
+          toast.success("Email sent successfully.");
+        }}
+        className="mt-10 flex flex-col dark:text-black"
+      >
+        <input
+          type="email"
+          name="senderEmail"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          maxLength={500}
+          placeholder="Your email."
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
+    </motion.section>
+  );
 };
 
 export default Contact;
