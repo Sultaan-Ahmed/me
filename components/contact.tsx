02ae07f3174c9ba02c7c760a2b3b808ab472bc5e
sultaan-ahmed <sultanduet2019@gmail.com>
2023-11-08 08:46:44 +0600

    Debug contact form section

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail.ts";
+import { sendEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 const Contact = () => {
